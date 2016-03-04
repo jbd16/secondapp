fe845af1d8b5fa06ea7ad494a61197a0d44d16f3
Jane Roberts <janer27@gmail.com>
2016-03-04 14:36:33 -0800

    added color and price fields to products with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,9 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#  cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#  Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
+
+Product.create(:name => "another try", :description => "go again")
