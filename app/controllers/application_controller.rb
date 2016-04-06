43bbd650e2165f99c3ef8fb63821000ac7967a9d
Jane Roberts <janer27@gmail.com>
2016-04-05 17:59:14 -0700

    five point three

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+ 
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
+
+
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
